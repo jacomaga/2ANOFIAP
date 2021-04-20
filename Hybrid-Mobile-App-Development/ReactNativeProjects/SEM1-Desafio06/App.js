01b7943e82ad83d6691402537606e2c4938fb3df
Jaco Magalhães <jacomagalhaes@gmail.com>
2021-04-20 13:16:42 -0300

    20/04

--- Hybrid-Mobile-App-Development/ReactNativeProjects/SEM1-Desafio06/App.js
+++ Hybrid-Mobile-App-Development/ReactNativeProjects/SEM1-Desafio06/App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StatusBar, View, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
+import image01 from './assets/1.png';
+import image02 from './assets/2.png';
+import image03 from './assets/3.png';
+import image04 from './assets/4.png';
+
+
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      image: image01
+    }
+
+  }
+
+  render() {
+   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Image
+        source={this.state.image}
+        style= {styles.image} >
+      </Image>
+
+      <View style={styles.separator} />
+      <Button
+        title="Python"
+        onPress={() => this.setState({"image":image01})}
+        color="black"
+      />
+      <View style={styles.separator}  />
+      <Button
+        title="NodeJS"
+        onPress={() => this.setState({"image":image02})}
+        color="black"
+      />
+      <View style={styles.separator} />
+      <Button
+        title="Golang"
+        onPress={() => this.setState({"image":image03})}
+        color="black"
+      />
+      <View style={styles.separator} />
+      <Button
+        title="Ruby"
+        onPress={() => this.setState({"image":image04})}
+        color="black"
+      />
+      <StatusBar />
+
     </View>
-  );
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'gray',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20
   },
-});
+  image: {
+    alignSelf: 'center',
+    height: 200,
+    width: 200
+
+  },
+  separator: {
+    marginBottom: 15,
+  }
+})
